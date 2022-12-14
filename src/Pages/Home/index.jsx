899bbda05c878488a54b45ba5073802aacddbd66
work4code <work4inventions@gmail.com>
2022-12-14 22:10:32 +0530

    add the code of the navbar

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import './home.scss'
+import Logo from "../../../src/assets/icon.svg";
+import { HiOutlineChevronUpDown, HiShoppingCart } from "react-icons/hi2";
+import { AiOutlineSearch } from "react-icons/ai";
+import { BiFilter } from "react-icons/bi";
 
 function Home() {
   return (
-    <div>
-     Home 
+    <div className='nav-container'>
+      <div className='main-container'>
+        <img src={Logo} alt="logo" className="logo" />
+        <p className='Da-Otto'> Da Otto</p>
+        <HiOutlineChevronUpDown size={25} className="down-up-arrow" />
+        <div className='search'>
+          <AiOutlineSearch className='search-icon' />
+          <input type="text" className='search-input' maxLength="50" size="15" placeholder="Search for Restaurants  (Press Enter to search)" />
+        </div>
+        <div className='filter-container'>
+          <BiFilter size={30} />
+        </div>
+        <div className='shopping-container'>
+          <HiShoppingCart size={25} />
+        </div>
+      </div>
     </div>
   )
 }
